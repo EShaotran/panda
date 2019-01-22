76a696bc765884d50a821e01d88b8da0be4f3553
Exbow <ethan@energize.ai>
2019-01-21 21:09:45 -0800

    Added static messaging

--- RC/RC_Toyota.py
+++ RC/RC_Toyota.py
@@ -1,6 +1,7 @@
 #Remote-Controlled Car Script
 #Integrated for Toyota Prius Prime 2018
 #--Ethan Shaotran--
+#__William_____
 
 import sys
 from panda import Panda
@@ -11,7 +12,7 @@ import tty
 import select
 from hexdump import hexdump
 
-KPH_to_MPH = 0.621371
+KPH_to_MPH = 1.60934
 
 class CAR:
   PRIUS = "TOYOTA PRIUS 2017"
@@ -33,7 +34,7 @@ class ECU:
   DSU = 1 # driving support unit
   APGS = 2 # advanced parking guidance system
 
-enable_camera = False
+enable_camera = True
 enable_dsu = True
 enable_apg = False
 
@@ -78,14 +79,19 @@ STATIC_MSGS = [
   (0x470, ECU.DSU, (CAR.HIGHLANDER, CAR.HIGHLANDERH), 1,  100, '\x00\x00\x01\x79'),
   (0x4CB, ECU.DSU, (CAR.PRIUS, CAR.RAV4H, CAR.LEXUS_RXH, CAR.RAV4, CAR.COROLLA, CAR.HIGHLANDERH, CAR.HIGHLANDER), 0, 100, '\x0c\x00\x00\x00\x00\x00\x00\x00'),
 
-  (0x292, ECU.APGS, (CAR.PRIUS), 0,   3, '\x00\x00\x00\x00\x00\x00\x00\x9e'),
-  (0x32E, ECU.APGS, (CAR.PRIUS), 0,  20, '\x00\x00\x00\x00\x00\x00\x00\x00'),
-  (0x396, ECU.APGS, (CAR.PRIUS), 0, 100, '\xBD\x00\x00\x00\x60\x0F\x02\x00'),
-  (0x43A, ECU.APGS, (CAR.PRIUS), 0, 100, '\x84\x00\x00\x00\x00\x00\x00\x00'),
-  (0x43B, ECU.APGS, (CAR.PRIUS), 0, 100, '\x00\x00\x00\x00\x00\x00\x00\x00'),
-  (0x497, ECU.APGS, (CAR.PRIUS), 0, 100, '\x00\x00\x00\x00\x00\x00\x00\x00'),
-  (0x4CC, ECU.APGS, (CAR.PRIUS), 0, 100, '\x0D\x00\x00\x00\x00\x00\x00\x00'),
+  # (0x292, ECU.APGS, (CAR.PRIUS), 0,   3, '\x00\x00\x00\x00\x00\x00\x00\x9e'),
+  # (0x32E, ECU.APGS, (CAR.PRIUS), 0,  20, '\x00\x00\x00\x00\x00\x00\x00\x00'),
+  # (0x396, ECU.APGS, (CAR.PRIUS), 0, 100, '\xBD\x00\x00\x00\x60\x0F\x02\x00'),
+  # (0x43A, ECU.APGS, (CAR.PRIUS), 0, 100, '\x84\x00\x00\x00\x00\x00\x00\x00'),
+  # (0x43B, ECU.APGS, (CAR.PRIUS), 0, 100, '\x00\x00\x00\x00\x00\x00\x00\x00'),
+  # (0x497, ECU.APGS, (CAR.PRIUS), 0, 100, '\x00\x00\x00\x00\x00\x00\x00\x00'),
+  # (0x4CC, ECU.APGS, (CAR.PRIUS), 0, 100, '\x0D\x00\x00\x00\x00\x00\x00\x00'),
 ]
+
+TARGET_IDS = [0x340, 0x341, 0x342, 0x343, 0x344, 0x345,
+              0x363, 0x364, 0x365, 0x370, 0x371, 0x372,
+              0x373, 0x374, 0x375, 0x380, 0x381, 0x382,
+              0x383]
 
 goLeft = False
 goRight = False
@@ -130,7 +136,7 @@ def getKeys():
 
 def updateFrame():
   global Frame
-  #threading.Timer(0.01, updateFrame).start()
+  threading.Timer(0.01, updateFrame).start()
   Frame += 1
 
 def checksum(addr,arr):
@@ -140,14 +146,16 @@ def checksum(addr,arr):
   Sum = 0
   for msg in arr:
     Sum += msg
-  return hex((IDH+IDL+Len+Sum) & 0xFF)
+  return str(hex((IDH+IDL+Len+Sum) & 0xFF))
 
 def send(addr, dat):
   global panda
+  print(addr)
   panda.can_send(addr, dat, 0)
 
-def send(addr, dat, bus):
+def sendbus(addr, dat, bus):
   global panda
+  print(addr)
   panda.can_send(addr, dat, bus)
 
 
@@ -176,7 +184,6 @@ def updateTorque(dat):
 #EDITORS
 def sendStatics():
   global Frame
-  threading.Timer(0.01, sendStatics).start()
   for (addr, ecu, cars, bus, fr_step, vl) in STATIC_MSGS:
     if Frame % fr_step == 0 and ecu in fake_ecus and car_fingerprint in cars:
       # special cases
@@ -190,55 +197,29 @@ def sendStatics():
           # 0x48a has a 8 preceding the counter
           cnt += 1 << 7
         vl += chr(cnt)
-      print(bus)
-      send(addr,vl,bus)
-  updateFrame()
+      #print(bus)
+      sendbus(addr,vl,bus)
 
 def sendAccel():
   global goAccel, goBrake
-  threading.Timer(0.01, sendAccel).start()
-  if goAccel:
-    accRate = 900  #-20k to 20k
-  elif goBrake:
-    accRate = -400
+  if not goAccel and not goBrake:
+    #BLANK: apply_acc=0,pcm_cancel=?,standstill_req=False,relStandstill=True
+    vals = [0,0,255,int('11000000',2),0,0,0]
+    vals = vals + [int(checksum(0x343,vals),16)]
+    #print(vals)
+    send(0x343,str(bytearray(vals)))
   else:
-    accRate = 0
-  values = [accRate >> 8, accRate & 0xff, 255, int('11000000',2), 0, 0, 0]
-  values = values + [int(calc_checksum(values, len(values)+1))]
-  print(bytearray(values))
-  send(0x343, str(bytearray(values))) #m/s2, 0x343 (ACC_CONTROL), 0x245 (GAS_PEDAL), 0x200 (GAS_COMMAND)
+    if goAccel:
+      accRate = 900  #-20k to 20k
+    elif goBrake:
+      accRate = -400
 
-str_cntr = 0
-def sendSteer2():
-  global goRight, goLeft, str_cntr
-  str_cntr+=1
-  if str_cntr == 64:
-    str_cntr = 0
-  threading.Timer(0.01, sendSteer2).start()
-  if goLeft:
-    steer = 0 - 200
-    req = 1
-  elif goRight:
-    steer = 0 + 200
-    req = 1
-  else:
-    steer = 0
-    req = 0
-
-  values = {
-    "LKA_STATE": 0,
-    "STEER_REQUEST": 0x1, #0,1
-    "COUNTER": 0,
-    "SET_ME_1": 1,
-    "STEER_TORQUE_CMD": bin(steer),
-    "CHECKSUM": bin(str_cntr),
-  }
-  values = [0, req, str_cntr, 1, steer >> 8, steer & 0xff, 0]
-  values = values + [calc_checksum(values, len(values)+1)]
-  send(0x2E4, str(bytearray(values))) #0x2E4 (STEERING_LKA), 0x266 (STEERING_IPAS), 0x167 (STEERING_IPAS_COMMA)
+    values = [accRate >> 8, accRate & 0xff, 255, int('11000000',2), 0, 0, 0]
+    values = values + [int(checksum(0x343,values),16)]
+    send(0x343, str(bytearray(values)))
 
 def goAcc():
-  threading.Timer(0.1, goAcc).start()
+  threading.Timer(0.03, goAcc).start()
   values = [0x05,0xa0,0x63,0xc0,0x00,0x00,0x00]
   values = values + [checksum(0x343,values)]
   send(0x343,str(bytearray(values)))
@@ -247,33 +228,59 @@ def goAcc():
   #\IDH\IDL   h\bushex2,len\0,bushex1\00\00\msg\
 
 cnt = 0
-def goStr():
-  global cnt
-  threading.Timer(0.1, goStr).start()
-  if cnt>62: cnt=-1
-  cnt+=1
-  mid = cnt
-  lenmid = int(len(str(bin(mid))))-2
-  midstr = str(bin(mid)[2:])
-  while (lenmid<6): #"0b" takes 2 spaces
-    midstr = '0'+midstr
-    lenmid+=1
-  b1 = '1' + str(midstr) + '1'
-  b1 = int(b1,2)
-  torque=64256
-  b2 = torque >> 8
-  b3 = torque & 0xFF
-  b4 = 0x80
-  arr = [b1,b2,b3,b4]
-  b5 = checksum(0x2E4,arr)
-  arr = arr + [int(b5,16)]
-  print(arr)
-  send(0x2E4, str(bytearray(arr)))
+def sendSteer():
+  global cnt, goRight, goLeft
+  if not goRight and not goLeft:
+    #BLANK, app_steer=0, app_steer_req=0
+    values = [Frame&0xff,0,0,0x80]
+    values = values + [int(checksum(0x2E4,values),16)]
+    send(0x2E4,str(bytearray(values)))
+  else:
+    if cnt>62: cnt=-1
+    cnt+=1
+    mid = cnt
+    lenmid = int(len(str(bin(mid))))-2
+    midstr = str(bin(mid)[2:])
+    while (lenmid<6): #"0b" takes 2 spaces
+      midstr = '0'+midstr
+      lenmid+=1
+    b1 = '1' + str(midstr) + '1'
+    b1 = int(b1,2)
+    torque=64256
+    b2 = torque >> 8
+    b3 = torque & 0xFF
+    b4 = 0x80
+    arr = [b1,b2,b3,b4]
+    b5 = checksum(0x2E4,arr)
+    arr = arr + [int(b5,16)]
+    send(0x2E4, str(bytearray(arr)))
+
+# def sendIPASSteer():
+#   send()
+
+def sendVideoTarget():
+  for addr in TARGET_IDS:
+    arr = ["!BBBBBBB", Frame/10 & 0xff, 0x03, 0xff, 0x00, 0x00, 0x00, 0x00]
+    send(addr,str(bytearray(arr)))
+
+
+def sendUI():
+  sound1 = 0 #off 'chimeRepeated' audible_alert
+  sound2 = 0 #off 'beepSingle','chimeSingle','chimeDouble' audible_alert
+  steer = 0 #off 'steerRequired' hud_alert
+  vals = [1,1,0x0c,0x2c,0x38,0x02,1,1,sound1,sound2,steer]
+  send(0x412, str(bytearray(vals)))
+
+
+def sendFCW():
+  fcw = 0 #off 'fcw' hud_alert
+  vals = [fcw,0x20,0x10,0x80]
+  send(0x411, str(bytearray(vals)))
 
 def HighBeam():
   threading.Timer(0.1, HighBeam).start()
   vals = [0x29,0,0x60,0x20,00,00,0x07,0xc5]
-  send(0x614, str(bytearray(vals)))
+  sendcan(0x614, str(bytearray(vals)), 1)
 
 spdcnt = -3
 def ChangeSpeed():
@@ -284,12 +291,22 @@ def ChangeSpeed():
   threading.Timer(0.01, ChangeSpeed).start()
   vals = [0,0,0,0,spdcnt,06,66]
   vals = vals + [int(checksum(0xB4,vals),16)]
-  print(vals)
+  #print(vals)
   send(0xB4, str(bytearray(vals)))
 
 
+def runLoop():
+  global frame
+  threading.Timer(0.01, runLoop).start()
+  # sendStatics()
+  sendSteer()
+  if Frame%3==0: sendAccel()
+  #sendIPASSteer()
+  #if Frame%10==0: sendVideoTarget()
+  #if Frame%100==0: sendUI()
+  # if Frame%100==0: sendFCW()
 
-
+  updateFrame()
 
 panda = None
 def main():
@@ -298,28 +315,16 @@ def main():
   panda.set_safety_mode(Panda.SAFETY_ALLOUTPUT)
   panda.can_clear(0)
 
-  sendStatics()
-  #updateFrame()
+  runLoop()
 
-  # thread = Thread(target = getKeys)
-  # thread.start()
+  thread = Thread(target = getKeys)
+  thread.start()
 
-  #ChangeSpeed()
-  #HighBeam()
-  #goStr()
-  #goAcc()
-
-  #sendSteer()
-  #sendAccel()
-  #sendBrake()
-
-  # while True:
-  #   can_recv = panda.can_recv()
-  #   for address, _, dat, src in can_recv:
-  #     if src == 0 and address == 0xB4: #SPEED
-  #       updateMPH(dat) #kph
-  #     if src == 0 and address == 0x24: #KINEMATICS
-  #       updateTorque(dat)
+  while True:
+    can_recv = panda.can_recv()
+    for address, _, dat, src in can_recv:
+      if src == 0 and address == 0xB4: #SPEED
+        updateMPH(dat) #kph
 
 
 if __name__ == "__main__":
